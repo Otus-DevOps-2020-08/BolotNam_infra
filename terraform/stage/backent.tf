080aa1d9417552e1a149fbf22af985d6b010734e
Bolot Nam <bobastik1982@gmail.com>
2020-10-19 14:08:17 +0300

    do dynamic inventory

--- terraform/stage/backent.tf
+++ terraform/stage/backent.tf
@@ -4,8 +4,8 @@ terraform {
     bucket     = "nbt-bucket"
     region     = "ru-central1"
     key        = "prod/prod.tfstate"
-    access_key = "access_key
-    secret_key = "secret_key"
+    access_key = "pOYu-2TTLioyv7nqLBwF"
+    secret_key = "g1zgyUqYNZEfHfdYLmTzQnL8VgvbHYr-9fA9xIQJ"
 
 
     skip_region_validation      = true
